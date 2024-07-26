c242c8615da6b062230559f8fd2c35557b3748fe
iamjoker021 <iamjoker021@gmail.com>
2024-07-26 15:24:36 +0530

    style: Add Layout and style to Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,15 @@ export default function Header() {
         <>
             <header>
                 <h1>Memory Card Game</h1>
-                <ul>
-                    <li>Score: </li>
-                    <li>Best Score: </li>
+                <ul className="score-board">
+                    <li>
+                        <span>Score:</span>
+                        <span>0</span>
+                    </li>
+                    <li>
+                        <span>Best Score:</span>
+                        <span>0</span>
+                    </li>
                 </ul>
             </header>
             <p>Get points by clicking am image which is not already clicked</p>
