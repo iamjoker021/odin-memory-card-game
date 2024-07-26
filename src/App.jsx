61b4a87bf09944267ccf46d88ff87b28e4d193cc
iamjoker021 <iamjoker021@gmail.com>
2024-07-26 15:12:35 +0530

    feat: Add Header Page to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Header'
 
 function App() {
 
   return (
     <>
-      <h1>Memory Card Game</h1>
+      <Header />
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>Memory Card Game</h1>
+                <ul>
+                    <li>Score: </li>
+                    <li>Best Score: </li>
+                </ul>
+            </header>
+            <p>Get points by clicking am image which is not already clicked</p>
+        </>
+    )
+}
